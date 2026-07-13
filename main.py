1b44372051ed57d0d08d149ad58ed0da3e1e30b8
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2026-07-14 00:01:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,82 @@
 from ai.script import create_script
+from ai.parser import parse_scenes
+
 from video.render import create_video
+
 
 
 def main():
 
-    print("🤖 Создание сценария...")
+
+    print(
+        "🤖 Создание сценария..."
+    )
+
 
     script = create_script()
 
-    print(script)
 
 
-    print("🎬 Создание видео...")
+    print(
+        "\n📄 Получен сценарий:\n"
+    )
 
-    video = create_video(script)
 
-    print("Готово:", video)
+    print(
+        script
+    )
+
+
+
+    print(
+        "\n🧩 Разбор сцен..."
+    )
+
+
+    scenes = parse_scenes(
+        script
+    )
+
+
+
+    print(
+        f"Найдено сцен: {len(scenes)}"
+    )
+
+
+
+    for i, scene in enumerate(
+        scenes,
+        start=1
+    ):
+
+        print(
+            f"\nСцена {i}"
+        )
+
+        print(
+            "Текст:",
+            scene["text"]
+        )
+
+        print(
+            "Поиск:",
+            scene["search"]
+        )
+
+
+
+    print(
+        "\n🎬 Создание видео..."
+    )
+
+
+    create_video(
+        scenes
+    )
 
 
 
 if __name__ == "__main__":
+
     main()
