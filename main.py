1b09a7f849b40821a8cae43aa9b7ee16acd4fa1c
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2026-07-13 21:56:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from ai.script import create_script
+from video.render import create_video
 
+
+def main():
+
+    print("🤖 Создание сценария...")
+
+    script = create_script()
+
+    print(script)
+
+
+    print("🎬 Создание видео...")
+
+    video = create_video(script)
+
+    print("Готово:", video)
+
+
+
+if __name__ == "__main__":
+    main()
