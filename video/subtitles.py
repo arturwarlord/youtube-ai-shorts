e8aa7a49c1b967ceead9cb73d46c9330694018d6
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2026-07-13 22:40:29 +0300

    Update subtitles.py

--- video/subtitles.py
+++ video/subtitles.py
@@ -1,1 +1,16 @@
+from moviepy import TextClip
 
+
+def create_caption(text):
+
+    caption = TextClip(
+        text,
+        font_size=80,
+        color="white",
+        stroke_color="black",
+        stroke_width=3,
+        method="caption",
+        size=(900,None)
+    )
+
+    return caption
