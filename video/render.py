b6b2330dc0dc10c1d9526279dcd9bcfbcee733db
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2026-07-13 22:25:52 +0300

    Update render.py

--- video/render.py
+++ video/render.py
@@ -1,39 +1,46 @@
 import os
 import requests
+import asyncio
 import edge_tts
+
+from moviepy import (
+    VideoFileClip,
+    AudioFileClip,
+    CompositeVideoClip,
+    TextClip
+)
 
 
 PEXELS_KEY = os.getenv("PEXELS_KEY")
 
 
-async def voice(text):
+async def create_voice(text):
 
-    communicate = edge_tts.Communicate(
+    voice = edge_tts.Communicate(
         text,
         "ru-RU-DmitryNeural"
     )
 
-    await communicate.save(
-        "voice.mp3"
+    await voice.save(
+        "output/voice.mp3"
     )
 
 
 
-def create_video(script):
-
-    print("Ищем видео...")
-
+def download_video(query):
 
     headers = {
         "Authorization": PEXELS_KEY
     }
 
 
-    url = "https://api.pexels.com/videos/search"
+    url = (
+        "https://api.pexels.com/videos/search"
+    )
 
 
     params = {
-        "query": "nature",
+        "query": query,
         "per_page": 1
     }
 
@@ -48,14 +55,26 @@ def create_video(script):
     data = r.json()
 
 
-    video_url = (
+    link = (
         data["videos"][0]
         ["video_files"][0]
         ["link"]
     )
 
 
-    video = requests.get(video_url)
+    video = requests.get(link)
+
+
+    with open(
+        "output/source.mp4",
+        "wb"
+    ) as f:
+
+        f.write(video.content)
+
+
+
+def create_video(script):
 
 
     os.makedirs(
@@ -64,13 +83,65 @@ def create_video(script):
     )
 
 
-    with open(
-        "output/background.mp4",
-        "wb"
-    ) as f:
-        f.write(video.content)
+    print("🎙 Создаём голос")
 
 
-    print("Видео скачано")
+    text = script.split(
+        "TEXT:"
+    )[1].split(
+        "SEARCH:"
+    )[0]
 
-    return "output/background.mp4"
+
+    asyncio.run(
+        create_voice(text)
+    )
+
+
+    print("🎥 Скачиваем видео")
+
+
+    download_video(
+        "science technology"
+    )
+
+
+    print("🎬 Монтаж")
+
+
+    video = VideoFileClip(
+        "output/source.mp4"
+    )
+
+
+    audio = AudioFileClip(
+        "output/voice.mp3"
+    )
+
+
+    video = video.subclipped(
+        0,
+        min(
+            video.duration,
+            audio.duration
+        )
+    )
+
+
+    video = video.resized(
+        height=1920
+    )
+
+
+    final = video.with_audio(
+        audio
+    )
+
+
+    final.write_videofile(
+        "output/short.mp4",
+        fps=30
+    )
+
+
+    return "output/short.mp4"
