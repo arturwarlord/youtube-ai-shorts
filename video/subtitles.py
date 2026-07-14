1df120e9e9010ae7547d17abd1455b2927ec880c
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2026-07-14 17:53:06 +0300

    Update subtitles.py

--- video/subtitles.py
+++ video/subtitles.py
@@ -1,16 +1,64 @@
-from moviepy import TextClip
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+import textwrap
+
+from moviepy import ImageClip
 
 
-def create_caption(text):
+FONT_PATH = "assets/fonts/Montserrat-ExtraBold.ttf"
 
-    caption = TextClip(
-        text,
-        font_size=80,
-        color="white",
-        stroke_color="black",
-        stroke_width=3,
-        method="caption",
-        size=(900,None)
+WIDTH = 1080
+HEIGHT = 1920
+
+
+def create_subtitle(text, duration):
+
+    img = Image.new(
+        "RGBA",
+        (WIDTH, HEIGHT),
+        (0, 0, 0, 0)
     )
 
-    return caption
+    draw = ImageDraw.Draw(img)
+
+    font = ImageFont.truetype(
+        FONT_PATH,
+        78
+    )
+
+    # автоматический перенос строк
+    lines = textwrap.fill(
+        text.upper(),
+        width=18
+    )
+
+    bbox = draw.multiline_textbbox(
+        (0, 0),
+        lines,
+        font=font,
+        align="center"
+    )
+
+    tw = bbox[2] - bbox[0]
+    th = bbox[3] - bbox[1]
+
+    x = (WIDTH - tw) / 2
+    y = HEIGHT * 0.68
+
+    # черная обводка
+    stroke = 5
+
+    draw.multiline_text(
+        (x, y),
+        lines,
+        font=font,
+        fill="white",
+        align="center",
+        stroke_width=stroke,
+        stroke_fill="black"
+    )
+
+    return ImageClip(
+        np.array(img),
+        duration=duration
+    )
