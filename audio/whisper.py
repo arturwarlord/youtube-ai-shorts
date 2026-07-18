5d8aa52560eeacd8aadb03a9609fa4c907a3fb75
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2026-07-18 20:22:13 +0300

    Update whisper.py

--- audio/whisper.py
+++ audio/whisper.py
@@ -1,1 +1,73 @@
+import whisper
 
+
+MODEL = None
+
+
+
+def get_model():
+
+    global MODEL
+
+
+    if MODEL is None:
+
+        print("🧠 Загрузка Whisper модели...")
+
+        MODEL = whisper.load_model(
+            "base"
+        )
+
+
+    return MODEL
+
+
+
+
+
+def transcribe_audio(
+        audio_file
+):
+
+    model = get_model()
+
+
+    print(
+        "🎧 Распознавание голоса..."
+    )
+
+
+    result = model.transcribe(
+        audio_file,
+        language="ru",
+        word_timestamps=True
+    )
+
+
+    words = []
+
+
+    for segment in result["segments"]:
+
+        for word in segment["words"]:
+
+            words.append({
+
+                "word":
+                    word["word"].strip(),
+
+                "start":
+                    word["start"],
+
+                "end":
+                    word["end"]
+
+            })
+
+
+    print(
+        f"✅ Найдено слов: {len(words)}"
+    )
+
+
+    return words
