d14903cf3b5ce356ac97f81dbb63ca6a852ceb93
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2026-07-18 19:53:05 +0300

    Update music_selector.py

--- video/music_selector.py
+++ video/music_selector.py
@@ -1,1 +1,33 @@
+def select_music_style(text):
 
+    text = text.lower()
+
+
+    if any(word in text for word in [
+        "мозг",
+        "наука",
+        "учёные",
+        "космос",
+        "технологии"
+    ]):
+        return "dark ambient cinematic"
+
+
+    if any(word in text for word in [
+        "страш",
+        "ужас",
+        "тайна",
+        "монстр"
+    ]):
+        return "horror dark"
+
+
+    if any(word in text for word in [
+        "история",
+        "древн",
+        "война"
+    ]):
+        return "epic documentary"
+
+
+    return "cinematic ambient"
