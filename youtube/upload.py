f58e5c32c3287b85036b48670a5ca28d9836d767
arturwarlord <69643781+arturwarlord@users.noreply.github.com>
2026-07-22 20:48:05 +0300

    Update upload.py

--- youtube/upload.py
+++ youtube/upload.py
@@ -1,1 +1,12 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
 
+SCOPES = [
+    "https://www.googleapis.com/auth/youtube.upload"
+]
+
+flow = InstalledAppFlow.from_client_secrets_file(
+    "credentials.json",
+    SCOPES
+)
+
+credentials = flow.run_local_server(port=0)
